20e48b76e5f15cd120b7143bb7fde3a003e1c2f2
jaesungdhl <48365618+jaesungdhl@users.noreply.github.com>
2019-05-13 01:57:52 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public/stylesheets"));
+
+app.get("/", (req,res) => {
+    res.render("landing");
+})
+
+app.get("/ourstory", (req,res) => {
+    res.render("OurStory");
+})
+
+app.listen(process.env.port || 3000, () =>{
+    console.log("Server started running.")
+})
